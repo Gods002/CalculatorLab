32d3720e59fd6d85f30d9ba8e0c00aacc854705d
peetakarn <peetakarn.peach@me.com>
2017-08-30 00:10:24 +0700

    test1234

--- CPE200Lab1/CPE200Lab1/MainForm.cs
+++ CPE200Lab1/CPE200Lab1/MainForm.cs
@@ -90,7 +90,7 @@ namespace CPE200Lab1
             }
             if(lblDisplay.Text.Length is 8)
             {
-                lblDisplay.Text = "Error";
+                lblDisplay.Text = "Error22";
                 return;
             }
             isAllowBack = true;
